3f68211633b79be1ec0a0882e2e5e80691e34d05
Derek Lawless <dlawless@gmail.com>
2016-02-20 11:00:43 -0600

    Enable port setting for server.js

--- server.js
+++ server.js
@@ -3,11 +3,18 @@ var app = express();
 var accountService = require('./services/account');
 var bodyParser = require('body-parser');
 
+app.set('port', (process.env.PORT || 8080));
+
 app.use(bodyParser.json());
 
 // Account
+app.get('/', function(req, res){
+  res.send("hello");
+});
 app.post('/api/account', accountService.create);
 app.get('/api/account/:accountID', accountService.fetch);
 app.put('/api/account/:accountID', accountService.update);
 
-app.listen(8080);
+app.listen(app.get('port'), function() {
+  console.log('Node up and running on port %d Captain!', app.get('port'));
+});
